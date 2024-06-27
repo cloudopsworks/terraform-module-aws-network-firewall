2d1c873e9a6234b92c71543c97fcaaa022e93023
Cristian Beraha <cristian@cloudops.works>
2024-06-27 18:57:17 -0300

    chore: initial version network firewall

---
+++ main.tf
@@ -0,0 +1,52 @@
+##
+# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
+#            On GitHub: https://github.com/cloudopsworks
+#            Distributed Under Apache v2.0 License
+#
+
+module "nfw" {
+  source                                   = "terraform-aws-modules/network-firewall/aws"
+  version                                  = "~> 1.0"
+  name                                     = "nfw-${local.system_name}"
+  create                                   = var.create
+  create_logging_configuration             = var.logging.enabled
+  logging_configuration_destination_config = var.logging.configurations
+  create_policy                            = var.create_policy
+  create_policy_resource_policy            = var.create_policy_resource_policy
+  delete_protection                        = var.delete_protection
+  firewall_policy_arn                      = var.firewall_policy_arn
+  policy_encryption_configuration = {
+    key_id = aws_kms_key.this.key_id
+    type   = "AWS_OWNED_KMS_KEY"
+  }
+  policy_name                               = "nfw-pol-${local.system_name}"
+  policy_resource_policy_actions            = var.policy.resource.policy_actions
+  policy_resource_policy_principals         = var.policy.resource.policy_principals
+  policy_stateful_default_actions           = var.policy.stateful.default_actions
+  policy_stateful_engine_options            = var.policy.stateful.engine_options
+  policy_stateful_rule_group_reference      = var.policy.stateful.rule_group_reference
+  policy_stateless_custom_action            = var.policy.stateless.custom_action
+  policy_stateless_default_actions          = var.policy.stateless.default_actions
+  policy_stateless_fragment_default_actions = var.policy.stateless.fragment_default_actions
+  policy_stateless_rule_group_reference     = var.policy.stateless.rule_group_reference
+  policy_tags                               = local.all_tags
+  vpc_id                                    = var.vpc_id
+  subnet_mapping                            = var.subnet_mapping
+  tags                                      = local.all_tags
+}
+
+resource "aws_kms_key" "this" {
+  description             = "KMS key for Network Firewall Policy - ${local.system_name}"
+  deletion_window_in_days = 10
+  key_usage               = "ENCRYPT_DECRYPT"
+  enable_key_rotation     = true
+
+  tags = merge({
+    name = "nfw-kms-${local.system_name}"
+  }, local.all_tags)
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/nfw-kms-${local.system_name}"
+  target_key_id = aws_kms_key.this.key_id
+}
---
+++ variables-nfw.tf
@@ -0,0 +1,90 @@
+##
+# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
+#            On GitHub: https://github.com/cloudopsworks
+#            Distributed Under Apache v2.0 License
+#
+
+variable "create" {
+  type    = bool
+  default = true
+}
+
+variable "logging" {
+  type = object({
+    enabled        = optional(bool, false)
+    configurations = optional(any, [])
+  })
+  default = {
+    enabled        = false
+    configurations = []
+  }
+}
+
+variable "create_policy" {
+  type    = bool
+  default = true
+}
+
+variable "create_policy_resource_policy" {
+  type    = bool
+  default = false
+}
+
+variable "delete_protection" {
+  type    = bool
+  default = true
+}
+
+variable "policy" {
+  type = object({
+    resource = object({
+      policy_actions    = optional(list(string), [])
+      policy_principals = optional(list(string), [])
+    })
+    stateful = object({
+      default_actions      = optional(list(string), [])
+      engine_options       = optional(any, {})
+      rule_group_reference = optional(any, {})
+    })
+    stateless = object({
+      custom_action            = optional(any, {})
+      default_actions          = optional(list(string), [])
+      fragment_default_actions = optional(list(string), [])
+      rule_group_reference     = optional(any, {})
+    })
+  })
+  default = {
+    resource = {
+      policy_actions    = []
+      policy_principals = []
+    }
+    stateful = {
+      default_actions      = []
+      engine_options       = {}
+      rule_group_reference = {}
+    }
+    stateless = {
+      custom_action            = {}
+      default_actions          = []
+      fragment_default_actions = []
+      rule_group_reference     = {}
+    }
+  }
+}
+
+variable "firewall_policy_arn" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_mapping" {
+  type = list(object({
+    subnet_id       = string
+    ip_address_type = optional(string, null)
+  }))
+  default = []
+}
